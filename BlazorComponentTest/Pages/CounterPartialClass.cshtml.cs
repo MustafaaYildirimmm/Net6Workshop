1eea79ac40bf3f9fe2e7b474479ddc1e1cbdd582
mustafa yıldırım <mustafa.yildirim7@outlook.com>
2021-11-26 23:42:59 +0300

    web app eklendi

---
+++ BlazorComponentTest/Pages/CounterPartialClass.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorComponentTest.Pages
+{
+    public class CounterPartialClassModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
